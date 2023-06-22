e72b4558f535d1cf445c070f2ae073d43c5b6688
Seysmologic <dwingssp@gmail.com>
2023-06-22 11:49:26 +0300

    Class1

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import math
+
+sec_day = 3600*24
+sec_year = 365*sec_day
+
+print("Seconds in a day = ", sec_day, "\nSeconds in a year = ", sec_year)
+
+a = 10
+b = 15
+
+print("a + b =", a+b, "\na - b =", a-b, "\na / b =", a/b, "\na // b =", a//b, "\na * b =",  a*b)
+
+
+#planet_age = int(input("How many Earth years in the Planet's: "))
+#planet_name = input("Name of the planet: ")
+#print(planet_name, " year lasts ", planet_age*3600*24, " seconds")
+
+
+age_month = int(input("Enter the age in month = "))
+print(age_month/12 > 18)
+
+
+
+
