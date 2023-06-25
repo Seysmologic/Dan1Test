50fe969530c96e2bdc099057ee7e23774feddbf8
Seysmologic <dwingssp@gmail.com>
2023-06-25 13:14:08 +0300

    Class3

--- Class2.py
+++ Class2.py
@@ -1,8 +1,23 @@
-x = list(map(str, [input('Car manufacturer'), input('Year'), input('Engine')]))
+#x = list(map(str, [input('Car manufacturer'), input('Year'), input('Engine')]))
+#
+#print(f'Car manufacturer is {x[0]} \nManufactured in {x[1]} \nEngine type is {x[2]}')
+#
+#print('Car manufacturer is', x[0], '\nManufactured in', x[1], '\nEngine type is', x[2])
+#
+#x.append('100$')
+#print(x)
 
-print(f'Car manufacturer is {x[0]} \nManufactured in {x[1]} \nEngine type is {x[2]}')
 
-print('Car manufacturer is', x[0], '\nManufactured in', x[1], '\nEngine type is', x[2])
 
-x.append('100$')
-print(x)
+
+
+
+number = int(input('Enter the number '))
+if number % 2 == 0:
+    print('Number is Even')
+else:
+    print('Number is Odd')
+
+
+
+
